1eba4aae2c6b6e5d58d173a4c7f0aaeedec80918
ishan <scifyfuntime@gmail.com>
2022-05-09 10:06:11 +0530

    Complete

--- sd.py
+++ sd.py
@@ -27,18 +27,29 @@ print( "Sample data mean is", sample_mean)
 sample_stdev = statistics.stdev(mean_list) 
 print( "Sample data standard deviation is", sample_stdev)  
 
-df2 = mean_list
-mean = statistics.mean(df2)
-
-fig = ff.create_distplot([df2], ["reading_time"], show_hist=False)
-fig.add_trace(go.Scatter(x=[mean, mean], y=[0,1], mode="lines", name='Mean'))
-fig.show()
+df2 = pd.read_csv("data.csv")
+data2 = df["reading_time"].tolist()
+sample1_mean = statistics.mean(data2)
+print("Mean of sample 1 is", sample1_mean)
 
 first_std_deviation_start, first_std_deviation_end = sample_mean-sample_stdev, sample_mean+sample_stdev
 second_std_deviation_start, second_std_deviation_end = sample_mean-(2*sample_stdev), sample_mean+(2*sample_stdev)
 third_std_deviation_start, third_std_deviation_end = sample_mean-(3*sample_stdev), sample_mean+(3*sample_stdev)
 
-
 print("std1",first_std_deviation_start, first_std_deviation_end)
 print("std2",second_std_deviation_start, second_std_deviation_end)
 print("std3",third_std_deviation_start,third_std_deviation_end)
+
+fig = ff.create_distplot([mean_list], ["reading_time"], show_hist=False)
+fig.add_trace(go.Scatter(x=[sample1_mean, sample1_mean], y=[0, 1], mode="lines", name="Mean of sample 1"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 1], mode="lines", name="Standard deviation 1 start"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 1], mode="lines", name="Standard deviation 1 end"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 1], mode="lines", name="Standard deviation 2 start"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 1], mode="lines", name="Standard deviation 2 end"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_start,third_std_deviation_start], y=[0,1], mode="lines", name="Standard deviation 3 start"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end], y=[0,1], mode="lines", name="Standard deviation 3 end"))
+fig.show()
+
+z_score = (sample1_mean - pop_mean)/sample_stdev
+
+print("z score of the data is", z_score)
